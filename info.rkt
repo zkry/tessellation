54ea8f768c2bb5a6ff77cfe0a58a6d01a44b8b02
Zac Romero <zacromero@posteo.net>
2019-09-13 20:50:11 +0200

    Updaet dependencies.

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
 (define collection "tessellation")
 (define scribblings '(("scribblings/tessellation.scrbl" ())))
-(define deps '("base"))
+(define deps '("base" "metapict"))
 (define build-deps '("scribble-lib"))
